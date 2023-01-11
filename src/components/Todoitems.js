221140d3b30869a4624ae4d7efa3051a80668110
Calebbii <biicaleb316@gmail.com>
2023-01-11 11:34:32 +0300

    Updated the todoitems file

---
+++ src/components/Todoitems.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Todoitems() {
+  return (
+    <div>
+      <ul>
+        <li>Ideation of a Open end </li>
+        <li>Design the Project</li>
+        <li>Divide work among the team</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Todoitems
